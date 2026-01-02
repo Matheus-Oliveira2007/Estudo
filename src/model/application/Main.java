bd890794f5c354ed4217629e2158367d1c80878a
Matheus <deoliveiramatheus@gmail.com>
2026-01-02 12:02:40 -0300

    Criando função main

--- src/model/application/Main.java
+++ src/model/application/Main.java
@@ -1,9 +1,25 @@
 package model.application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter accont data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Holder: ");
+		String holder = sc.nextLine();
+		System.out.print("Initital balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
 
 	}
 
