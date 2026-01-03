a8ddf5e330c7bd62d54da3d29d8e6bfeca7d51aa
Matheus <deoliveiramatheus@gmail.com>
2026-01-02 21:34:40 -0300

    Resultado final do programa

--- src/model/application/Main.java
+++ src/model/application/Main.java
@@ -3,13 +3,16 @@ package model.application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import model.AccontExcepition.BusinessException;
+import model.entities.Accont;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter accont data");
 		System.out.print("Number: ");
 		int number = sc.nextInt();
@@ -21,6 +24,26 @@ public class Main {
 		System.out.print("Withdraw limit: ");
 		double withdrawLimit = sc.nextDouble();
 
+		Accont accont = new Accont(number, holder, balance, withdrawLimit);
+		System.out.println();
+		sc.nextLine();
+		
+		System.out.print("Enter amount for withdraw: ");
+		double withdraw = sc.nextDouble();
+		
+		try {
+			accont.withdraw(withdraw);
+			System.out.printf("New balance: %.2f" + accont.getBalance());
+
+		} catch (BusinessException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		} finally {
+			sc.close();
+		}
+	}
+	
+	public void metodo1() {
+		
 	}
 
 }
